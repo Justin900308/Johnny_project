0794cb884a77aeb364102bfe255c47efa73935a2
Justin900308 <wow3987@gmail.com>
2024-11-17 20:39:39 -0800

    Add files via upload

--- Johnny_server.py
+++ Johnny_server.py
@@ -1,58 +1,8 @@
-# import socket
-# import numpy as np
-# import pickle
-#
-#
-# def start_server():
-#     # Create a socket object (IPv4, TCP)
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#     # Bind to an available IP and port
-#     host = '0.0.0.0'  # Listen on all available network interfaces
-#     port = 12345
-#     server_socket.bind((host, port))
-#
-#     # Listen for incoming connections
-#     server_socket.listen(5)
-#     print(f"Server listening on {host}:{port}...")
-#
-#     while True:
-#         # Accept a new client connection
-#         client_socket, client_address = server_socket.accept()
-#         print(f"Connection from {client_address} established.")
-#
-#         # Receive data from the client
-#         data = b""
-#         packet = client_socket.recv(4096)
-#         data += packet
-#
-#
-#         # Deserialize the received data into a NumPy array
-#         received_array = pickle.loads(data)
-#         print(f"Current pos:\n{received_array}")
-#
-#         # Process the array (e.g., calculate the sum)
-#         result = received_array
-#         print(f"Pos to be sent: {result}")
-#
-#         # Send the result back to the client
-#         client_socket.send(pickle.dumps(result))
-#
-#         # Close the client connection
-#         client_socket.close()
-#
-#
-# if __name__ == "__main__":
-#     start_server()
-
 from time import time
 from threading import Thread, Event
 from time import sleep
 from threading import Lock
 import matplotlib.pyplot as plt
-import socket
-import numpy as np
-import pickle
 import socket
 import numpy as np
 import pickle
@@ -83,7 +33,7 @@ class Server:
         self.stop = False
         self.P_des = np.array([75.4, -8.9, 8.6])
         self.mover = []
-        self.Des_coord = []
+        self.Des_coord = np.array([750.4, -80.9, 80.6])
 
         self.Get_data()
         sleep(5)
@@ -95,7 +45,12 @@ class Server:
 
     # self.live_plot(True)
 
-
+    def onclick(self,event):
+        print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+              ('double' if event.dblclick else 'single', event.button,
+               event.x, event.y, event.xdata, event.ydata))
+        self.Des_coord = np.array([event.xdata, event.ydata, 8.6])
+        print(self.Des_coord)
 
     @threaded
     def test(self):
@@ -126,33 +81,32 @@ class Server:
 
             # Deserialize the received data into a NumPy array
             self.mover = pickle.loads(data)
-            print(f"Current pos:{self.mover}")
+            #print(f"Current pos:{self.mover}")
             # received_array = pickle.loads(data)
             # print(f"Current pos:{received_array}")
 
             # Process the array (e.g., calculate the sum)
 
-            print(f"Pos to be sent: {self.mover}")
+           # print(f"Pos to be sent: {self.mover}")
             # result = received_array
             # print(f"Pos to be sent: {result}")
 
             # Send the result back to the client
-            client_socket.send(pickle.dumps(self.mover))
+            client_socket.send(pickle.dumps(self.Des_coord))
 
             # Close the client connection
             client_socket.close()
 
             self.Johnny_name = list(self.mover)[0]
 
-
-
-
+            v = self.mover[self.Johnny_name][2]
+            #print(v)
             if self.stop == True:
                 break
 
     @threaded
     def live_plot(self, blit=False):
-        t = np.linspace(0, 50., num=100)
+        t = np.linspace(0, 50., num=500)
         Vx = np.zeros(t.shape)
         Vr = np.zeros(t.shape)
         x = np.zeros(t.shape)
@@ -190,17 +144,9 @@ class Server:
 
             xx0 = self.mover[self.Johnny_name][0]
             th0 = self.mover[self.Johnny_name][1]
-            #mdi = fig.canvas.mpl_connect('button_press_event', self.onclick)
-            if self.Des_coord == []:
-                # print("List is empty")
-                Pdes = self.P_des * 10
-
-            else:
-                # print(self.P_des)
-                Pdes = self.Des_coord[0:2]
-                self.P_des = self.Des_coord
-            # Pdes = self.P_des*10
-            # print(self.Des_coord)
+            mdi = fig.canvas.mpl_connect('button_press_event', self.onclick)
+            Pdes = self.Des_coord[0:2]
+            print(self.Des_coord)
             Vx = np.concatenate((Vx[1:], [np.linalg.norm(v)]))
             Vr = np.concatenate((Vr[1:], [r0[2]]))
             x = np.concatenate((x[1:], [xx0[0]]))
@@ -208,7 +154,7 @@ class Server:
             th = np.concatenate((th[1:], [th0[2]]))
 
             line1.set_data(x, y)
-            #line2.set_data(Pdes[0], Pdes[1])
+            line2.set_data([Pdes[0]], [Pdes[1]])
 
             k += 0.11
             if blit:
@@ -217,7 +163,7 @@ class Server:
 
                 # redraw just the points
                 ax1.draw_artist(line1)
-                #ax1.draw_artist(line2)
+                ax1.draw_artist(line2)
 
                 # coords = plt.ginput(5)
                 # fill in the axes rectangle
